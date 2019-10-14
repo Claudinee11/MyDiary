495e03733d9e4bb890eee48c909faf2787653066
Claudinee11 <niyoclaudine10@gmail.com>
2019-10-15 10:54:47 +1400

    created endpoint of add new entry [Finishes #169136195]

--- server/routers/diaryRouters.js
+++ server/routers/diaryRouters.js
@@ -7,6 +7,7 @@ const app = express.Router();
 
 app.get('/api/v1/entries', myDiary.viewallEntry);
 app.get('/api/v1/entries/:id', myDiary.getspecificEntry);
+app.post('/api/v1/entries', myDiary.addnewEntry);
 
 
 export default app; 
