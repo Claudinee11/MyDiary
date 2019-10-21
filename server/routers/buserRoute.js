99304eb88743575435d3a3849630bcf37c01995c
Claudinee11 <niyoclaudine10@gmail.com>
2019-10-22 05:15:52 +1400

    created login endpoint [Finishes #169264021]

--- server/routers/buserRoute.js
+++ server/routers/buserRoute.js
@@ -4,7 +4,8 @@ import myDiary from '../controllers/userControllers';
 const app = express.Router();
 
 
-app.post('/api/v1/auth/signup', myDiary.createAccount)
+app.post('/api/v1/auth/signup', myDiary.createAccount);
+app.post('/api/v1/auth/signin',myDiary.diaryLogin)
 
 
 export default app;
