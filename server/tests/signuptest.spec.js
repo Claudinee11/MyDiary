461322d4c8b2129425302982c6e3a529942aa143
Claudinee11 <niyoclaudine10@gmail.com>
2019-10-22 07:39:50 +1400

    created test of sign up [Finishes #169268695]

---
+++ server/tests/signuptest.spec.js
@@ -0,0 +1,77 @@
+
+import chai, { expect } from 'chai';
+
+import chaiHTTP from 'chai-http';
+
+import userInformation from '../models/userTest';
+
+import app from '../../index';
+
+chai.use(chaiHTTP);
+
+
+describe('POST user creating an account with invalid email', () => {
+    it('user should return status of bad requested', (done) => {
+        chai
+            .request(app)
+            .post('/api/v1/auth/signup')
+            .set('Accept', 'application/json')
+            .send(userInformation[1])
+            .end((err, res) => {
+                expect(res.status).to.equals(400);
+                expect(res.body).to.be.an('object');
+                expect(res.body.status).to.equals(400);
+
+                done();
+            });
+    });
+});
+describe('POST user creating an account without password', () => {
+    it('user should return status of bad requested', (done) => {
+        chai
+            .request(app)
+            .post('/api/v1/auth/signup')
+            .set('Accept', 'application/json')
+            .send(userInformation[2])
+            .end((err, res) => {
+                expect(res.status).to.equals(400);
+                expect(res.body).to.be.an('object');
+                expect(res.body.status).to.equals(400);
+
+                done();
+            });
+    });
+});
+describe('POST user creating an account without firstname', () => {
+    it('user should return status of bad requested', (done) => {
+        chai
+            .request(app)
+            .post('/api/v1/auth/signup')
+            .set('Accept', 'application/json')
+            .send(userInformation[3])
+            .end((err, res) => {
+                expect(res.status).to.equals(400);
+                expect(res.body).to.be.an('object');
+                expect(res.body.status).to.equals(400);
+
+                done();
+            });
+    });
+});
+describe('POST user creating an account without lastname', () => {
+    it('user should return status of bad requested', (done) => {
+        chai
+            .request(app)
+            .post('/api/v1/auth/signup')
+            .set('Accept', 'application/json')
+            .send(userInformation[4])
+            .end((err, res) => {
+                expect(res.status).to.equals(400);
+                expect(res.body).to.be.an('object');
+                expect(res.body.status).to.equals(400);
+
+                done();
+            });
+    });
+});
+;
