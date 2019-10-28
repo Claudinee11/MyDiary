a10421ca1e477ea06279bdc788f06a9a77765deb
Claudinee11 <niyoclaudine10@gmail.com>
2019-10-29 06:43:43 +1400

    Created entry test [Finishes #169385240]

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    env: {
+        es6: true,
+        node: true
+    },
+    extends: [
+        airbnb - base
+    ],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module"
+    },
+    rules: {
+        'linebreak-style': 0,
+        'space-before-blocks': 0,
+        'no-trailing-spaces': 0, 
+
+
+    },
+};
